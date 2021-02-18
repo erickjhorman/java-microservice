763b3a1cce07eaa49e3b08765b9481a755315f10
Erick <erickjhorman@gmail.com>
2021-02-18 11:57:16 -0500

    I added the eureka configuration to each microservice in server-config server to allow them to connect to eureka

--- customer-service/src/main/java/academy/digitallab/store/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/academy/digitallab/store/customer/CustomerServiceApplication.java
@@ -2,8 +2,11 @@ package academy.digitallab.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
--- service-product/service-product/src/main/java/academy/digitallab/store/product/ServiceProductApplication.java
+++ service-product/service-product/src/main/java/academy/digitallab/store/product/ServiceProductApplication.java
@@ -2,8 +2,10 @@ package academy.digitallab.store.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceProductApplication {
 
 	public static void main(String[] args) {
