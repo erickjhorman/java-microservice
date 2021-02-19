4f8374a077ae8d3428d298122b9e37b279e61469
Erick <erickjhorman@gmail.com>
2021-02-19 10:13:07 -0500

    I added a hystrix fallback to customer micro service

--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("customer-service")
-@RequestMapping("/customers")
+@FeignClient(value = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+//@RequestMapping("/customers")
 public interface CustomerClient {
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/customers/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
 }
--- shopping-service/src/main/java/academy/digitallab/store/shopping/model/Customer.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/model/Customer.java
@@ -1,8 +1,10 @@
 package academy.digitallab.store.shopping.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Customer {
     private Long id;
     private String numberID;
